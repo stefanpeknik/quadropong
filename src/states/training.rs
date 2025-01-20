39960869280643bee446cf51ccd939d01bb4f3bc
gzvv <nespora@protonmail.com>
2025-01-20 14:51:10 +0100

    feat: add TODO screen for training lobby

---
+++ src/states/training.rs
@@ -0,0 +1,108 @@
+use super::menu::Menu;
+use super::traits::{HasOptions, ListEnum, Render, State, Update};
+use super::utils::render::{draw_inner_rectangle, draw_outer_rectangle, render_list};
+
+use crossterm::event::KeyCode;
+use ratatui::style::Stylize;
+use ratatui::Frame;
+
+#[derive(Clone)]
+pub enum Options {
+    TODO,
+}
+
+impl ListEnum for Options {
+    fn list() -> Vec<Self> {
+        vec![Options::TODO]
+    }
+}
+
+impl std::fmt::Display for Options {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self {
+            Options::TODO => write!(f, "TODO"),
+        }
+    }
+}
+
+#[derive(Clone)]
+pub struct Training {
+    options: Vec<Options>,
+    selected: usize,
+}
+
+impl Training {
+    pub fn new() -> Self {
+        Self {
+            options: Options::list(),
+            selected: 0,
+        }
+    }
+}
+
+impl HasOptions for Training {
+    fn next(&mut self) {
+        self.selected = (self.selected + 1) % self.options.len();
+    }
+
+    fn previous(&mut self) {
+        if self.selected == 0 {
+            self.selected = self.options.len() - 1;
+        } else {
+            self.selected -= 1;
+        }
+    }
+}
+
+impl State for Training {
+    fn clone_box(&self) -> Box<dyn State> {
+        Box::new(self.clone())
+    }
+}
+
+#[async_trait::async_trait]
+impl Update for Training {
+    async fn update(
+        &mut self,
+        key_code: Option<KeyCode>,
+    ) -> Result<Option<Box<dyn State>>, std::io::Error> {
+        if let Some(key_code) = key_code {
+            match key_code {
+                KeyCode::Up => self.previous(),
+                KeyCode::Down => self.next(),
+                KeyCode::Enter => match self.options[self.selected] {
+                    // TODO: Implement this
+                    _ => {}
+                },
+                KeyCode::Esc => {
+                    return Ok(Some(Box::new(Menu::new(1))));
+                }
+                _ => {}
+            };
+        }
+        Ok(Some(Box::new(self.clone())))
+    }
+}
+
+impl Render for Training {
+    fn render(&self, frame: &mut Frame) {
+        let outer_rect = draw_outer_rectangle(
+            frame,
+            " quadropong ",
+            vec![" Back ".into(), " <Esc> ".blue().bold()],
+        );
+
+        let inner_rect = draw_inner_rectangle(frame, outer_rect);
+
+        render_list(
+            frame,
+            &self
+                .options
+                .iter()
+                .map(|x| x.to_string())
+                .collect::<Vec<String>>(),
+            self.selected,
+            inner_rect,
+        );
+    }
+}
