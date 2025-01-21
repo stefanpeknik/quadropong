d83805cca4d5433b5b5fde37c2a311a0ae8c2a2f
Stefan Peknik <xpeknik@fi.muni.cz>
2025-01-21 13:51:41 +0100

    feat(lobby_ui): screw dtos for now

--- src/game_models/dto/mod.rs
+++
@@ -1,7 +0,0 @@
-mod ball_dto;
-mod game_dto;
-mod player_dto;
-
-pub use ball_dto::BallDto;
-pub use game_dto::GameDto;
-pub use player_dto::PlayerDto;
--- src/game_models/mod.rs
+++ src/game_models/mod.rs
@@ -1,5 +1,4 @@
 pub mod ball;
 pub mod client_input;
-pub mod dto;
 pub mod game;
 pub mod player;
